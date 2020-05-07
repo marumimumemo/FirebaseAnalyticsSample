ee5080c89eba3555c51bc221aa247e5a6e7e48a5
satoshi.marumoto <satoshi.marumoto@st-ventures.jp>
2020-05-07 18:43:33 +0900

    コードを追加

--- FirebaseAnalyticsSample/ViewController.swift
+++ FirebaseAnalyticsSample/ViewController.swift
@@ -13,9 +13,36 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // クラッシュボタン
+        let button = UIButton(type: .roundedRect)
+        button.frame = CGRect(x: 20, y: 50, width: 100, height: 30)
+        button.backgroundColor = .red
+        button.setTitle("Crash", for: [])
+        button.addTarget(self, action: #selector(self.crashButtonTapped(_:)), for: .touchUpInside)
+        view.addSubview(button)
     }
-
+    
+    @IBAction func buttonTapped() {
+        Analytics.logEvent(AnalyticsEventSelectContent, parameters: [
+            AnalyticsParameterItemID: "id-001",
+            AnalyticsParameterItemName: "登録ボタン",
+            AnalyticsParameterContentType: "コンテンツタイプ"
+        ])
+    }
+    
+    @IBAction func buttonTapped2() {
+        Analytics.logEvent("share_image", parameters: [
+            "name": "シェア" as NSObject,
+            "full_text": "シェア" as NSObject
+        ])
+    }
+    
+    @IBAction func crashButtonTapped(_ sender: AnyObject) {
+        _ = [0, 1][2]
+        Crashlytics.setValue("crashButton_tapped", forKey: "crashButton_tapped_action")
+        Crashlytics.crashlytics().log("crashButton_tappedで落ちました")
+        Crashlytics.crashlytics().setUserID("1234")
+    }
 
 }
 
